13b2fe2a47912a17b88f3c44e56288709db7a18c
ajones-tasktop <adam.jones@tasktop.com>
2017-06-05 12:18:44 -0700

    commit 3
    151589

---
+++ sample-payload.js
@@ -0,0 +1,11 @@
+{
+  "change_set": "String",
+  "formatted_id": "String",
+  "summary": "String",
+  "contributes_to": [
+    {
+      "location": "http://example.com",
+      "label": "Label"
+    }
+  ]
+}
